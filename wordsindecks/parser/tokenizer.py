d22b67c6078e8279ea9ec1e0f9059bfa1f8dfe67
Benoit <benoit.maillard@epfl.ch>
2021-02-21 10:38:51 +0100

    Use regex groups in order to exclude irrelevant characters

--- wordsindecks/parser/tokenizer.py
+++ wordsindecks/parser/tokenizer.py
@@ -16,20 +16,19 @@ class TokenKind(Enum):
     TEXT = auto()
     IGNORE = auto()
 
-# TODO enclose all regex in groups in order to have the option to exclude some characters ?
 TOKENIZE_RULES = [
-    (r'(?:\n|^)==*', TokenKind.HEADING_OPEN),
-    (r'==*\n', TokenKind.HEADING_CLOSE),
-    (r'\{\{', TokenKind.TEMPLATE_OPEN),
-    (r'\}\}', TokenKind.TEMPLATE_CLOSE),
-    (r'\|\|', TokenKind.TEMPLATE_SPLIT),
-    (r'\[\[', TokenKind.LINK_OPEN),
-    (r'\]\]', TokenKind.LINK_CLOSE),
-    (r'\n[\*\#\:\;]*', TokenKind.LIST_ITEM),
-    (r'<[A-Za-z][A-Za-z]*>', TokenKind.HTML_OPEN), # TODO add support for attributes
-    (r'</[A-Za-z][A-Za-z]*>', TokenKind.HTML_CLOSE),
-    (r'\n\n(?![=\*\#\:\;])', TokenKind.PBREAK),
-    (r'\n', TokenKind.IGNORE),
+    (r'(?:\n|^)(=+)', TokenKind.HEADING_OPEN),
+    (r'(=+)(?:\n)', TokenKind.HEADING_CLOSE),
+    (r'(\{\{)', TokenKind.TEMPLATE_OPEN),
+    (r'(\}\})', TokenKind.TEMPLATE_CLOSE),
+    (r'(\|\|)', TokenKind.TEMPLATE_SPLIT),
+    (r'(\[\[)', TokenKind.LINK_OPEN),
+    (r'(\]\])', TokenKind.LINK_CLOSE),
+    (r'(?:\n)([\*\#\:\;]+)', TokenKind.LIST_ITEM),
+    (r'(<[A-Za-z]+>)', TokenKind.HTML_OPEN), # TODO add support for attributes
+    (r'(</[A-Za-z]+>)', TokenKind.HTML_CLOSE),
+    (r'(\n\n(?![=\*\#\:\;]))', TokenKind.PBREAK),
+    (r'(\n)', TokenKind.IGNORE),
 ]
 
 class Token:
@@ -62,9 +61,7 @@ class Tokenizer:
                         tokens.append(Token(TokenKind.TEXT, orphan_chars, (pos - len(orphan_chars), pos)))
                         orphan_chars = ""
 
-                    # TODO should match.group() be replaced by "".join(match.groups()) to exclude
-                    # some characters (such as leading \n)?
-                    tokens.append(Token(kind, match.group(), match.span()))
+                    tokens.append(Token(kind, "".join(match.groups()), match.span()))
                     break
 
             if new_pos == pos:
